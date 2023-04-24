fd53701d4ebe54937ba53064001862156dc04e8f
EgonRasmussen <ecr@eucsyd.dk>
2023-04-24 16:27:23 +0200

    ok

--- TodoREST/Constants.cs
+++ TodoREST/Constants.cs
@@ -3,8 +3,6 @@
     public static class Constants
     {
         // DevTunnes url
-        //public static string RestUrl = $"<Dev Tunnel>/todoitems/{0}";
-
-        public static string RestUrl = "https://1mbc30x2-7245.euw.devtunnels.ms/todoitems/{0}";
+        public static string RestUrl = $"<Dev Tunnel>/todoitems/{0}";
     }
 }
