40c00ff07328d5e3e37bd6f02dfea8703e6733d7
EgonRasmussen <ecr@eucsyd.dk>
2023-04-24 16:30:08 +0200

    ok

--- TodoREST/Constants.cs
+++ TodoREST/Constants.cs
@@ -4,8 +4,6 @@
     {
         // With DevTunnels, Url is not persistent
 
-        //public static string RestUrl = "<Your DevTunnel url>/{0}";
-
-        public static string RestUrl = "https://1mbc30x2-7245.euw.devtunnels.ms/todoitems/{0}";
+        public static string RestUrl = "<Your DevTunnel url>/todoitems/{0}";
     }
 }
